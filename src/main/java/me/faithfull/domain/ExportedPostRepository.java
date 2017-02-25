4e2b8de06dfb96e47ec89d5d1a1c09f7be33b308
Will Faithfull <will@sourcepulp.com>
2017-02-25 12:20:56 +0000

    @Primary

--- src/main/java/me/faithfull/domain/ExportedPostRepository.java
+++ src/main/java/me/faithfull/domain/ExportedPostRepository.java
@@ -1,5 +1,6 @@
 package me.faithfull.domain;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -8,6 +9,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
  * @author Will Faithfull
  */
 @RepositoryRestResource(exported = true, path = "posts")
+@Primary
 @PreAuthorize("hasRole('ADMINISTRATOR')")
 public interface ExportedPostRepository extends PagingAndSortingRepository<Post, Long> {
 }
